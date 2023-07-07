223936f8eea85ab2cd1f8137801e9740a2a78556
Abhijeet Kanase <chillgoziecoder@outlook.com>
2023-07-07 20:29:47 +0530

    Committed

---
+++ project-1/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Navigation from './components/navigation';
+import HeroSection from './components/Hero';
+
+const App = () => 
+{
+    return <div>
+        <Navigation />
+        <HeroSection />
+        
+    </div>;
+  
+}// component - capital A for app
+
+export default App;
+// in react to create a class,
+//  we call it className
